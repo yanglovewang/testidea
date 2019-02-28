dbe790fe8c64a15d944c7daa495c952f0c347138
yang <yangchengjuily@163.com>
2019-02-28 21:12:19 +0800

    提交

---
+++ ideatest/src/main/java/com/yang/ideatest/IdeatestApplication.java
@@ -0,0 +1,13 @@
+package com.yang.ideatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeatestApplication.class, args);
+	}
+
+}
